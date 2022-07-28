65b93906a760295aaebd9cf7fdccd2a14ed54daf
Beni Levi <benilevi2005@gmail.com>
2022-07-28 16:44:34 +0300

    Added sorting feature

--- src/components/LinerTable.js
+++ src/components/LinerTable.js
@@ -2,47 +2,53 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box } from '@mui/material'
 import LinerList from './companies.json'
+import Button from './Button'
 
 const tableHeaderCellStyle = { textAlign: "center", fontWeight: "bold", textDecoration: "underline" }
 
+
+
+// const [linerNameArray, setlinerName] = useState([])
+
 const LinerTable = () => {
-  const [data, setData] = useState('')
-  const getData = (val) => {
-    setData(val.target.value)
-  }
-
-  const [filteredList, setFilteredList] = useState([]);
-  // const filteredList = []
-  const filterData = () => {
-    // LinerList.forEach((item) => {
-    //   if (data === '') {
-    //     filteredList.push(item);
-    //   } else if (item.name.toLowerCase().trim().includes(data.trim().toLowerCase())) {
-
-    //     filteredList.push(item);
-    //   }
-    // })
-    return (data === '') ? LinerList : LinerList.filter((item) => item.name.toLowerCase().includes(data.trim().toLowerCase()))
-
-  }
-
+  const [globalList, setGlobalList] = useState(LinerList)
+  const [filteredList, setFilteredList] = useState(LinerList)
+  const [orderedList, setOrderedList] = useState(LinerList)
+  const [search, setSearch] = useState('')
+  const [input, setInput] = useState('')
 
   useEffect(() => {
-    // filterData();
-    setFilteredList(filterData());
-  }, [data])
+    setFilteredList((search === '') ? LinerList : LinerList.filter((item) => item.name.toLowerCase().includes(search.trim().toLowerCase())))
+    setInput('filter')
+  }, [search])
 
+  const order = () => {
+    setOrderedList(globalList.sort((a, b) => (a.name > b.name ? 1 : -1)))
+    setInput('order')
+
+  }
+  // useReducer()
   useEffect(() => {
-    console.log(data)
-  }, [data], filterData)
+    if (input === 'order') {
+      setGlobalList(orderedList)
+    }
+  }, [orderedList])
+  useEffect(() => {
+    if (input === 'filter') {
+      setGlobalList(filteredList)
+    }
+  }, [filteredList])
 
   return (
     <div>
-      <Box sx={{ alignItems: "right" }}>
-        <input type="text" id="test4" onChange={getData}></input>
-      </Box>
+      <div>
+        <Button text="Sort by Name" onClick={order} />
+        <Box sx={{ textAlign: "right" }}>
+          <input type="text" id="test4" onChange={(event) => { setSearch(event.target.value); }}></input>
+        </Box>
+      </div>
       <TableContainer component={Paper}>
-        <Table aria-label="simple table">
+        <Table aria-label="simple table" id="root">
           <TableHead>
             <TableRow sx={{ backgroundColor: "#66bb6a" }}>
               <TableCell sx={tableHeaderCellStyle}>Rank</TableCell>
@@ -54,7 +60,7 @@ const LinerTable = () => {
           <TableBody>
 
 
-            {filteredList.map((liner) => (
+            {globalList.map((liner) => (
               <TableRow key={liner.id}>
                 <TableCell sx={{ textAlign: "center" }}>{liner.id}</TableCell>
                 <TableCell sx={{ textAlign: "center" }}>{<img alt={liner.name} style={{ objectFit: 'scale-down', height: 60, width: 60 }} src={liner.logo} />}</TableCell>
